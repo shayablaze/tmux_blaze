f73f020bbdbebffedb7a38b4e88e3445992b6897
Ajzner, Yeshayahu Dov <YeshayahuDov.Ajzner@ca.com>
2018-05-18 16:02:58 +0300

    er

--- useful-scripts/qa_logs.zsh
+++ useful-scripts/qa_logs.zsh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-kubectl -n ns-jenkins exec -it  $(kubectl get pods -n ns-jenkins -l role=backend-worker -o yaml | grep "name: dpl-backend-worker-79-jenkins-qa*" | head -n 1 | cut -d':' -f2 | awk '{print $1}') -- sh -c "tail -f /var/log/blazemeter/blazemeter.log.json" | jq -r
+kubectl -n ns-jenkins exec -i  $(kubectl get pods -n ns-jenkins -l role=backend-worker -o yaml | grep "name: dpl-backend-worker-79-jenkins-qa*" | head -n 1 | cut -d':' -f2 | awk '{print $1}') -- sh -c "tail -f /var/log/blazemeter/blazemeter.log.json" | jq -r
