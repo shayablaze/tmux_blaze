171357f77eb3c24436424948ce8907f59debf747
Ajzner, Yeshayahu Dov <YeshayahuDov.Ajzner@ca.com>
2018-05-18 16:26:22 +0300

    erg

--- useful-scripts/me_bash.zsh
+++ useful-scripts/me_bash.zsh
@@ -1,4 +1,3 @@
 #!/bin/zsh
 
-#kubectl exec -i $(kubectl get pods -l role=bza -o jsonpath="{.items[0].metadata.name}") -- sh -c "cd /home/blazemeter/vol/www/a.blazemeter.com/; touch bla.txt; bash"
-kubectl exec -i dpl-bza-126-ajzye01-shayablaze-7cb77f6bc4-7l4d8 bash
+kubectl exec -it $(kubectl get pods -l role=bza -o jsonpath="{.items[0].metadata.name}") -- sh -c "cd /home/blazemeter/vol/www/a.blazemeter.com/; touch bla.txt; bash"
