c1570cff929dfada63c9ef956db6cda0e5c0fea6
Shaya Ajzner <shayasms128@gmail.com>
2018-05-21 12:24:13 +0300

    Update qa_mongo.zsh

--- useful-scripts/qa_mongo.zsh
+++ useful-scripts/qa_mongo.zsh
@@ -1,1 +1,1 @@
-kubectl -n ns-jenkins port-forward $(kubectl get pods -n ns-jenkins -l role=backend-worker -o yaml | grep "name: dpl-backend-worker-79-jenkins-qa*" | head -n 1 | cut -d':' -f2 | awk '{print $1}') 27018:27017
+kubectl -n ns-jenkins port-forward $(kubectl get pods -n ns-jenkins -l role=mongo -o yaml | grep "name: dpl-mongo-79-jenkins-qa*" | head -n 1 | cut -d':' -f2 | awk '{print $1}') 27016:27017
